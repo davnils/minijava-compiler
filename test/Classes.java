cc321251270c40dddde2e3ea8f2d4ea0dcafda34
davnils <nilsson.dd+github+gravatarobscurity@gmail.com>
2014-03-06 23:32:58 +0100

    Basic test cases.

---
+++ test/Classes.java
@@ -0,0 +1,23 @@
+class TheMainClass
+{
+  public static void main(String [] args)
+  {
+    System.out.println(2+3);
+  }
+}
+
+class Class2
+{
+  public int method(int a)
+  {
+    return 2;
+  }
+}
+
+class Class3
+{
+  public boolean method(int a, int b)
+  {
+    return false;
+  }
+}
---
+++ test/Hello.java
@@ -0,0 +1,7 @@
+class TheMainClass
+{
+  public static void main(String [] args)
+  {
+    System.out.println(2+3);
+  }
+}
