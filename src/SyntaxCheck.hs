75f01b2ebe5aacee09e4cd666df598bcd61fbcd8
davnils <nilsson.dd+github+gravatarobscurity@gmail.com>
2014-03-07 00:25:32 +0100

    Add syntax checking skeleton.

---
+++ src/SyntaxCheck.hs
@@ -0,0 +1,12 @@
+module SyntaxCheck where
+
+import AST
+
+tagVariables :: AST -> AST
+tagVariables = undefined
+
+checkVariableUsage :: AST -> True
+checkVariableUsage = undefined
+
+typeCheck :: AST -> True
+typeCheck = undefined
